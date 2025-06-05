055faa0bb3c9561706473b3629bc7b8da53c6929
doerikeh <doerikeh1@gmail.com>
2025-06-05 08:11:15 +0700

    change success

--- src/components/Success.tsx
+++ src/components/Success.tsx
@@ -3,7 +3,7 @@ import { Toast, ToastToggle } from "flowbite-react";
 
 export default function Success() {
   return (
-    <Toast className="absolute top-4 right-5 bg-green-50">
+    <Toast className="hidden lg:absolute top-4 right-5 bg-green-50">
       <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500 dark:bg-green-800 dark:text-green-200">
         <CheckIcon />
       </div>
